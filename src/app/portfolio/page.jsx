a879e5b75129077444fc8a250b35b1637d112ec7
Joachim Mwengi <kimwambua96@gmail.com>
2024-04-29 22:33:03 +0300

    started portfolio page design

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+"use client";
+import { motion, useScroll } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+import { useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "https://lama.dev",
+  },
+];
 
 const PortfolioPage = () => {
-  return (
-    <div>
-      Portfolio Page
-    </div>
-  )
-}
+  const ref = useRef();
 
-export default PortfolioPage
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-[600vh] relative" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          My Work
+        </div>
+        <div
+          className={`sticky top-0 flex h-screen gap-4 items-center overflow-hidden`}
+        >
+          <motion.div style={{ x }} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300 "></div>
+            {items.map((item) => (
+              <div
+                key={item.id}
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1>{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} alt="" fill />
+                  </div>
+                  <p>{item.desc}</p>
+                  <Link href={item.link}>
+                    <button>View Demo</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+        <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+          <h1>Do You Have A Project?</h1>
+          <div className="relative">
+            <svg viewBox="0 0 300 300">
+              <defs>
+                <path
+                  id="circlePath"
+                  d="M 150, 150 m-60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0"
+                />
+              </defs>
+            </svg>
+            <Link
+              href="/contact"
+              className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-shite rounded-full"
+            >
+              Let's Talk
+            </Link>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default PortfolioPage;
