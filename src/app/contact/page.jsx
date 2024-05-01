4c83a4c40aa2405ac31743def0010acadb0cf632
Joachim Mwengi <kimwambua96@gmail.com>
2024-05-01 17:42:21 +0300

    completed initial design

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
-  return (
-    <div>
-      Contact Page
-    </div>
-  )
-}
+  const greeting = "Say Hello";
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
-export default ContactPage
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, {
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+      })
+      .then(
+        (result) => {
+          setSuccess(true)
+          form.current.reset();
+        },
+        (error) => {
+          setError(true);
+        },
+      );
+  };
+
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <motion.div>
+            {greeting.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </motion.div>
+        </div>
+
+        {/* FORM CONTAINER */}
+        <form onSubmit={sendEmail} ref={form} className="h-1/2 lg:h-full sm:h-3/4 lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24">
+          <span>Dear Joachim,</span>
+          <textarea
+            id=""
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="message"
+            rows={6}
+          />
+          <span>My Mail Address is:</span>
+          <input
+            className="bg-transparent border-b-2 border-b-black outline-none"
+            type="email"
+            name="sender_email"
+
+          />
+          <span>Regards</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && <span className="text-green-600 font-semibold">Your Message Was Sent Successfully!</span>}
+          {error && <span className="text-red-600 font-semibold">Something Went Wrong!</span>}
+        </form>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ContactPage;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -44,7 +44,7 @@ const PortfolioPage = () => {
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
 
   return (
     <motion.div
@@ -67,38 +67,52 @@ const PortfolioPage = () => {
                 key={item.id}
                 className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
               >
-                <div className="flex flex-col gap-8 text-white">
-                  <h1>{item.title}</h1>
-                  <div className="relative">
+                <div className="flex flex-col gap-8 text-white my-20">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-4xl xl:text-6xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[360px]">
                     <Image src={item.img} alt="" fill />
                   </div>
-                  <p>{item.desc}</p>
-                  <Link href={item.link}>
-                    <button>View Demo</button>
+                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-md xl:w-[600px]">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className="flex justify-end ">
+                    <button  className="p-2 text-sm md:p-3 md:text-md lg:p-4 lg:text-lg bg-white text-gray-600 font-semibold m-1 rounded hover:bg-black hover:text-white transition-colors">View Demo</button>
                   </Link>
                 </div>
               </div>
             ))}
           </motion.div>
         </div>
-        <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-          <h1>Do You Have A Project?</h1>
-          <div className="relative">
-            <svg viewBox="0 0 300 300">
-              <defs>
-                <path
-                  id="circlePath"
-                  d="M 150, 150 m-60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0"
-                />
-              </defs>
-            </svg>
-            <Link
-              href="/contact"
-              className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-shite rounded-full"
-            >
-              Let's Talk
-            </Link>
-          </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-6xl">Do You Have A Project?</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px]"
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Full Stack Software Engineer
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex justify-center items-center"
+          >
+            Let's Talk
+          </Link>
         </div>
       </div>
     </motion.div>
