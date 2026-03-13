d41b0fc9e31e0e2c17d95ff84e9990f8a7de8c44
Joachim-Wambua <kimwambua96@gmail.com>
2026-03-13 11:37:43 +0100

    Updated Landing page text blurb

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,8 +11,7 @@ const Homepage = () => {
             src="/hero-img.png"
             alt="Hero Image"
             fill
-            // width={450}
-            // height={450}
+            priority // Prioritizes Loading for LCP elements
             className="object-contain"
           />
         </div>
@@ -21,19 +20,19 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* Title */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Innovative Web Solutions, Tailored for You
+            Building Digital Experiences, Engineered for Performance.
           </h1>
           {/* description */}
           <p className="md:text-lg">
-            Hi! I&lsquo;m Joachim, a dedicated software engineer blending
-            creativity and code to deliver stunning digital experiences that
-            resonate with users and drive success
+            Hi! I&apos;m Joachim, a full-stack software engineer blending
+          creativity and code to deliver high-quality web applications that
+          resonate with users.
           </p>
 
           <div className="flex w-full gap-4">
             <Link href="/portfolio">
               <button className="rounded-lg before:ease relative h-12 w-40 overflow-hidden border border-gray-500 bg-black text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-gray-700 hover:before:-translate-x-40">
-                <span relative="relative z-10">View My Work</span>
+                <span className="relative z-10">View My Work</span>
               </button>
             </Link>
             <Link href="/contact">
